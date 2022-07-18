5f64a9d1be7a4d910b2e8cf1370b6569f19d3261
khj <dunhill741@naver.com>
2022-07-18 15:10:56 +0900

    fix model schema

--- init_db.py
+++ init_db.py
@@ -26,9 +26,9 @@ columns = {
     "article_url": "TEXT",
     "excluded": "BLOB",
     "content": "TEXT",
-    "search_keyword": "TEXT",
+    # "search_keyword": "TEXT",
     "image_dirs": "TEXT",
-    "hash_id": "TEXT",
+    # "hash_id": "TEXT",
 }
 
 
@@ -39,13 +39,13 @@ if __name__ == "__main__":
         search_keyword = os.path.basename(file).split(".")[0]
         news_data = pd.read_excel(file)
         news_data["content"] = None
-        news_data["search_keyword"] = search_keyword
+        # news_data["search_keyword"] = search_keyword
         news_data["image_dirs"] = None
-        news_data["hash_id"] = None
+        # news_data["hash_id"] = None
         news_data.columns = list(columns.keys())
-        news_data["hash_id"] = news_data.apply(
-            lambda x: str(hash((x["press"], x["title"]))), axis=1
-        )
+        # news_data["hash_id"] = news_data.apply(
+        #     lambda x: str(hash((x["press"], x["title"]))), axis=1
+        # )
         news_data.columns = list(columns.keys())
         news_data.dropna(axis=0, inplace=True, subset=["article_url"])
         news_data.to_sql("news", con, if_exists="append", index=False, dtype=columns)
